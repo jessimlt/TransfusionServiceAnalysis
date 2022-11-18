7a60062e63e0bc581e12ccf35c41b536d028d224
Jessica Jones <118002868+jessimlt@users.noreply.github.com>
2022-11-18 05:05:38 -0600

    Add files via upload
    split into tabs

--- ui.R
+++ ui.R
@@ -3,23 +3,32 @@ library(shiny)
 
 # Define UI for application that draws a histogram
 shinyUI(
-  pageWithSidebar(
-    headerPanel("Transfusion Service Analysis"),
-    
-    sidebarPanel(
-      selectInput("locations", "Transfusion Locations", choices = c("", "BB", "C", "CRC")),
-      numericInput('glucose', 'Glucose mg/dl', 90, min = 50, max = 200, step = 5)
-    ),
-    
-    mainPanel(
-      plotOutput("g_if"),
-      h3('Result of prediction'),
-      h4('You entered '),
-      verbatimTextOutput("inputValue"),
-      h4('Which resulted in a prediction of '),
-      verbatimTextOutput("prediction")
-    )
-
   
+  fluidPage(
+    tabsetPanel(
+      tabPanel("tab1", fluid = TRUE,
+               sidebarLayout(
+                 sidebarPanel(
+                   selectInput("locations", "Transfusion Locations", choices = c("", "BB", "C", "CRC"))
+                   ),
+                 mainPanel(plotOutput("g_if")
+                           )
+                 )
+               ),
+      tabPanel("tab2", fluid = TRUE,
+               sidebarLayout(
+                 sidebarPanel(
+                   numericInput('glucose', 'Glucose mg/dl', 90, min = 50, max = 200, step = 5)
+                   ),
+                 mainPanel(h3('Result of prediction'),
+                           h4('You entered '),
+                           verbatimTextOutput("inputValue"),
+                           h4('Which resulted in a prediction of '),
+                           verbatimTextOutput("prediction")
+                 )
+               )
+      )
+    )
   )
 )
+
